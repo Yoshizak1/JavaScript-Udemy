87ff7beebfb357039ce470706c2b4a52fc30345c
João Pedro Yoshizaki <Yoshizaki02@hotmail.com>
2023-05-05 16:59:40 -0300

    update 16-20

---
+++ 19_switch/script.js
@@ -0,0 +1,13 @@
+let nome = "joão";
+
+switch(nome) {
+    case "joão":
+        console.log("O nome é João");
+        break;
+    case "Pedro":
+        console.log("O nome é Pedro");
+        break;
+    default:
+        console.log("O nome não foi encontrado");
+        break;
+}
