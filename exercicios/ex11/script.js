ad3a380bcb3dcb78c5edf967f55c1eab9f2d4a10
João Pedro Yoshizaki <Yoshizaki02@hotmail.com>
2023-05-05 21:43:20 -0300

    update 11 ao 13

---
+++ exercicios/ex11/script.js
@@ -0,0 +1,10 @@
+let idade = 18;
+let cnh = true;
+
+if(idade >= 18 && cnh == true ){
+    console.log("Sim, pode dirigir!");
+} else if (idade >= 18 && cnh == false) {
+    console.log("Não pode dirigir!")
+} else {
+    console.log("Não pode dirigir!")
+}
