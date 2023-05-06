5a796c2063f49a039d86dbe5de226fb9140f4326
João Pedro Yoshizaki <Yoshizaki02@hotmail.com>
2023-05-05 21:57:02 -0300

    update ex14-15

---
+++ exercicios/ex14/script.js
@@ -0,0 +1,7 @@
+for(let x = 10; x <= 50; i++) {
+    if(x % 2 == 0){
+        console.log(`O número ${x} é par`);
+    } else {
+        console.log(`O número ${x} é impar`);
+    }
+}
---
+++ exercicios/ex15/script.js
@@ -0,0 +1,14 @@
+let num = 22;
+let divisoes = 0;
+
+for(let x = 1; x <= num; x++){
+    if(num % x == 0){
+        divisoes++;
+    }
+}
+
+if(divisoes == 2){
+    console.log(`O número ${num} é primo`);
+} else {
+    console.log(`O número ${num} não é primo`);
+}
