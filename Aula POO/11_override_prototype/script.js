8c10f398c10312cf0760a2706bdb6514b36689ec
João Pedro Yoshizaki <Yoshizaki02@hotmail.com>
2023-05-16 09:48:33 -0300

    update POO 11 - 15

--- Aula POO/11_override_prototype/script.js
+++ Aula POO/11_override_prototype/script.js
@@ -9,9 +9,12 @@ class Cachorro {
     }
 }
 
+Cachorro.prototype.raca = 'SRD';
 Cachorro.prototype.patas = 4;
 
 let labrador = new Cachorro('Labrador', 'Amarelo');
-console.log(labrador);
+
 console.log(labrador.patas);
 labrador.latir();
+console.log(Cachorro.prototype.raca);
+console.log(labrador.raca);
