5c58e7f409eb7e818c54e05b2405dd03630ac385
João Pedro Yoshizaki <Yoshizaki02@hotmail.com>
2023-05-18 11:58:05 -0300

    update ex 01 - 08

---
+++ Aula Bug-Debug/05_exceptions/script.js
@@ -0,0 +1,9 @@
+function saudacao(nome) {
+    if(typeof nome != 'string') {
+        throw new Error("o parâmetro nome precisa ser uma string");
+    } else {
+        console.log(`Olá ${nome}`);
+    }
+} 
+
+saudacao("joão");
