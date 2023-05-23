a8b2cad95c375f923084323e55b41f1afc5e18c0
João Pedro Yoshizaki <Yoshizaki02@hotmail.com>
2023-05-22 21:12:34 -0300

    01-02

--- Aula Regex/01_regex_test/script.js
+++ Aula Regex/01_regex_test/script.js
@@ -2,3 +2,14 @@ const reg1 = new RegExp('bola');
 
 console.log(reg1.test("tem bola?"));
 console.log(reg1.test("não tem"));
+
+const reg2 = /bola/;
+
+let text = 'Tem bola na cesta';
+
+console.log(reg2.test("tem bola?"));
+console.log(reg2.test("não tem"));
+console.log(reg2.test(text));
+
+console.log(/quadrado/.test("Onde tem um quadrado"));
+console.log(/quadrado/.test("222222222222222222quadrado333333333333333333"));
