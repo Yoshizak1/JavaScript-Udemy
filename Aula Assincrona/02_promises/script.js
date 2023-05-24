bb819051d679e14a5819102f01fccfaa8d5a430b
João Pedro Yoshizaki <Yoshizaki02@hotmail.com>
2023-05-24 19:40:59 -0300

    update ex01-08

---
+++ Aula Assincrona/02_promises/script.js
@@ -0,0 +1,6 @@
+let p = promise.resolve(5);
+
+console.log("Outros códigos");
+console.log(p);
+
+p.then((value) => {console.log(`O valor é ${value}`)});
---
+++ Aula Assincrona/08_generator/script.js
@@ -0,0 +1,21 @@
+function* criadorId() {
+    let id = 0;
+    while(true) {
+        yield id++;
+    }
+}
+
+let criaid = criadorId();
+
+console.log(criaid.next().value);
+console.log(criaid.next().value);
+console.log(criaid.next().value);
+console.log(criaid.next().value);
+console.log(criaid.next().value);
+console.log(criaid.next().value);
+console.log(criaid.next().value);
+console.log(criaid.next().value);
+console.log(criaid.next().value);
+console.log(criaid.next().value);
+console.log(criaid.next().value);
+console.log(criaid.next().value);
